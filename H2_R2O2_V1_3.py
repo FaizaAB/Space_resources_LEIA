3915b5cb1f1ed45cf7fbfbcf70ad1eb77a37cc4b
Anton Morlock <antonmorlock@gmail.com>
2022-10-11 14:19:36 +0200

    added graphs

--- H2_R2O2_V1_3.py
+++ H2_R2O2_V1_3.py
@@ -9,7 +9,10 @@ Version 1.0
 forloops = False
 
 import numpy
-
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+import seaborn as sns
 import H2_Reactor_1
 from H2_Reactor_1 import *
 from electrolysis import *
@@ -351,9 +354,10 @@ def energy_as_func_of_ilmenite():
     L_energy_list = np.array(L_energy_list) 
     S_energy_list = np.array(S_energy_list) 
         
-    
+    energy_list = [  S_energy_list, L_energy_list, E_energy_list,R_energy_list, T_energy_list, X_energy_list]
 
     #Figure that plots the energy in function of ilmenite head grade
+    '''
     energy_as_func_of_ilmenite_figure = plt.figure(2)
     x = ilmenite_grade_list
     y = energy_as_func_of_ilmenite_list
@@ -378,12 +382,82 @@ def energy_as_func_of_ilmenite():
     plt.ylabel('Energy consumption [kWh/kg LOX]')
     plt.legend()
     plt.show()
+    '''
+    return ilmenite_grade_list, energy_list
     
     
 
-
+plt.rc('axes', axisbelow=True)
 energy_as_func_of_ilmenite()
 
+
+#used lists and variables for the bar plot
+viridis = cm.get_cmap('viridis', 12)
+pastel = sns.color_palette(palette = "muted", as_cmap=True)
+energy = [X_energy_per_kg_LOX,T_energy_per_kg_LOX,R_energy_per_kg_LOX,E_energy_per_kg_LOX,L_energy_per_kg_LOX,S_energy_per_kg_LOX]
+sum_energy = np.sum(energy)
+labels = np.around(energy/sum_energy*100, 1)
+energy_consumers_full = ["Excavation", "Transportation", "Reactor", "Electrolysis", "Liquefaction", "Storage"]
+colors_bars = ["tab:grey", "black", "tab:red", "tab:green",  "tab:blue", "tab:orange"]
+colors_bars = [viridis(0.1), viridis(0.2), viridis(0.45), viridis(0.6),  viridis(0.83), viridis(0.95)] 
+#colors_bars = [pastel[5], pastel[7], pastel[3], pastel[2],  pastel[0], pastel[8]]
+#colors_bars = ['#FEB144', pastel[7], '#FF6663', '#FDFD97',  '#9EC1CF', '#9EE09E']
+#colors_bars = ['black', '#8197a6', '#f1666a', '#00ae9d',  '#009bdb', '#1e3378']
+
+#used lists and variables for the stackplot
+ilmenite_grade_list, energy_list = energy_as_func_of_ilmenite()
+legend_stackplot = [ "Storage",  "Liquefaction", "Electrolysis","Reactor", "Transportation","Excavation"]
+colors_stackplot = [  "tab:orange",  "tab:blue", "tab:green", "tab:red","black","tab:grey" ]
+colors_stackplot = [  viridis(0.95),  viridis(0.83), viridis(0.6), viridis(0.45) , viridis(0.2),viridis(0.1)]
+#colors_stackplot = [pastel[8], pastel[0], pastel[2], pastel[3],  pastel[7], pastel[5]]
+#colors_stackplot = ['#9EE09E', '#9EC1CF', '#FDFD97', '#FF6663', pastel[7], '#FEB144']
+#colors_stackplot = ['#1e3378', '#009bdb', '#00ae9d', '#f1666a', '#8197a6', 'black']
+
+#create figure
+fig, (ax1, ax2) = plt.subplots(1, 2)
+
+
+#create stackplot
+#p2 = ax2.stackplot(ilmenite_grade_list, energy_list, colors = colors_stackplot, labels = legend_stackplot)
+
+barwidth = 12/len(ilmenite_grade_list)
+p2=ax2.bar(ilmenite_grade_list, energy_list[0], color=colors_stackplot[0], label='Storage', width = barwidth)
+p3=ax2.bar(ilmenite_grade_list, energy_list[1], bottom=energy_list[0], color=colors_stackplot[1], label='Liquefaction', width = barwidth)
+p4=ax2.bar(ilmenite_grade_list, energy_list[2], bottom=energy_list[0]+energy_list[1], color=colors_stackplot[2], label='Electrolysis', width = barwidth)
+p5=ax2.bar(ilmenite_grade_list, energy_list[3], bottom=energy_list[0]+energy_list[1]+energy_list[2], color=colors_stackplot[3], label='Reactor', width = barwidth)
+p6=ax2.bar(ilmenite_grade_list, energy_list[4], bottom=energy_list[0]+energy_list[1]+energy_list[2]+energy_list[3], color=colors_stackplot[4], label='Transportation', width = barwidth)
+p7=ax2.bar(ilmenite_grade_list, energy_list[5], bottom=energy_list[0]+energy_list[1]+energy_list[2]+energy_list[3]+energy_list[4], color=colors_stackplot[5], label='Excavation', width = barwidth)
+
+ax2.grid(axis = "y")
+ax2.set_title("B", loc="left", fontsize = 20)
+ax2.set_xlabel("Ilmenite %")
+ax2.set_ylabel('kWh/kg LOX')
+ax2.set_xlim((0.75,15.25))
+ax2.legend()
+
+#create bar plot
+p1 = ax1.bar(energy_consumers_full, energy, color = colors_bars)
+ax1.grid(axis = "y")
+ax1.set_title("A", loc = "left",  fontsize = 20)
+ax1.set_ylabel('kWh/kg LOX')
+
+index = -1
+for bar in p1:
+    index = index+1
+    width = bar.get_width()
+    height = bar.get_height()
+    x, y = bar.get_xy()
+    ax1.text(x+width/2,
+             y+height*1.01,
+             str(labels[index])+'%',
+             ha='center',
+             weight='bold')
+
+fig.autofmt_xdate()
+plt.setp(ax2.xaxis.get_majorticklabels(), rotation=0, ha="center", rotation_mode="anchor") 
+plt.suptitle('Energy comparison between different process steps')
+plt.show()
+plt.close()
 
 
 
